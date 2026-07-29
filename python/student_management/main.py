64cb837cde433181e757e1db4eea02c62e45fd55
Sadik <sadiksid398@gmail.com>
2026-07-29 18:46:51 +0900

    Day 1: Interactive basic menu completed

---
+++ python/student_management/main.py
@@ -0,0 +1,28 @@
+print("===== Student Management System =====")
+
+
+print("1. Add Student")
+print("2. Delete Student")
+print("3. Search Student")
+print("4. Show All Students")
+print("5. Exit")
+
+choice = input("Choose an option: ")
+
+
+if choice == "1":
+    print("Adding Student......")
+elif choice == "2":
+    print("Deleting Student.....")
+elif choice == "3":
+    print("Searching Student......")
+elif choice == "4":
+    print("Showing All Students.....")
+elif choice == "5":
+    print("exiting....")
+else:
+    print("Invalid Choice")
+    
+    
+    
+
